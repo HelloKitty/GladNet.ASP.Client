f1d6fee7001c8757e996ad59069d1fdb9c91c8dd
Andrew Blakely <andrew.blakely@ymail.com>
2016-06-25 02:50:10 -0500

    Simple IResponse Adapter

---
+++ src/GladNet.ASP.Client.Lib/ClientInterfaces/WebResponseNetworkMessageAdapter.cs
@@ -0,0 +1,18 @@
+﻿using GladNet.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GladNet.ASP.Client.Lib
+{
+	public class WebResponseNetworkMessageAdapter : IResponseMessage
+	{
+		public NetSendable<PacketPayload> Payload { get; }
+
+		public WebResponseNetworkMessageAdapter(PacketPayload payload)
+		{
+			Payload = new NetSendable<PacketPayload>(payload);
+		}
+	}
+}
