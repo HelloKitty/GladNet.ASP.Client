0c1dc452bd0eac6155087857d18a153c806270fe
Andrew Blakely <andrew.blakely@ymail.com>
2016-06-30 00:35:33 -0500

    Change WebRequestHandlerStrat Take Payload; Doc

--- src/GladNet.ASP.Client.Lib/ClientInterfaces/IWebRequestHandlerStrategy.cs
+++ src/GladNet.ASP.Client.Lib/ClientInterfaces/IWebRequestHandlerStrategy.cs
@@ -8,10 +8,17 @@ using System.Text;
 namespace GladNet.ASP.Client.Lib
 {
 	/// <summary>
-	/// 
+	/// Contract for web-request handling functionality.
+	/// Handles a serialized <see cref="PacketPayload"/> with the given request name.
 	/// </summary>
 	public interface IWebRequestHandlerStrategy
 	{
-		SendResult EnqueueRequest(byte[] serializedRequest, string requestName);
+		/// <summary>
+		/// Enqueues a webrequest to be handled with the provided serialized data.
+		/// </summary>
+		/// <param name="requestPayload">Request payload.</param>
+		/// <param name="requestName">String <see cref="Type"/> name of the <see cref="PacketPayload"/> type.</param>
+		/// <returns>Returns the result of the enqueued request.</returns>
+		SendResult EnqueueRequest(PacketPayload requestPayload);
 	}
 }
